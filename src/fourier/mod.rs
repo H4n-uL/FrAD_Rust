f88c1f206ca58973f38d3dd7c677bc4a7258f3b0
h4n-ul <jun061119@gmail.com>
2024-06-13 14:14:57 +0900

    $UPDATE$ :: Updated vector shape representing RAW PCM file

--- src/fourier/mod.rs
+++ src/fourier/mod.rs
@@ -2,15 +2,28 @@ pub mod backend;
 use backend::{u8pack, core::{dct, idct}};
 
 const DEPTHS: [i16; 6] = [12, 16, 24, 32, 48, 64];
-// const FLOAT_DR: [i16; 6] = [5, 5, 8, 8, 11, 11];
+const FLOAT_DR: [i16; 6] = [5, 5, 8, 8, 11, 11];
 
 pub fn analogue(pcm: Vec<Vec<f64>>, bits: i16, little_endian: bool) -> (Vec<u8>, i16) {
-    let freqs: Vec<Vec<f64>> = pcm.iter().map(|x| dct(x.clone())).collect();
+
+    let pcm_trans: Vec<Vec<f64>> = (0..pcm[0].len())
+        .map(|i| pcm.iter().map(|inner| inner[i]).collect())
+        .collect();
+
+    let freqs: Vec<Vec<f64>> = pcm_trans.iter().map(|x| dct(x.clone())).collect();
 
     let freqs_trans: Vec<Vec<f64>> = (0..freqs[0].len())
     .map(|i| freqs.iter().map(|inner| inner[i]).collect())
     .collect();
+
     let freqs_flat: Vec<f64> = freqs_trans.into_iter().flatten().collect();
+    let mut bx = DEPTHS.iter().position(|&x| x == bits).unwrap();
+    while freqs_flat.iter().max_by(|x, y| x.abs().partial_cmp(&y.abs()).unwrap()).unwrap().abs() > 2.0f64.powi(2.0f64.powi(FLOAT_DR[bx] as i32 - 1) as i32) {
+        if bx == DEPTHS.len() { panic!("Overflow with reaching the max bit depth."); }
+        bx += 1;
+    }
+
+
     let frad = u8pack::pack(freqs_flat, bits, !little_endian);
 
     return (frad, DEPTHS.iter().position(|&x| x == bits).unwrap() as i16);
@@ -24,11 +37,15 @@ pub fn digital(frad: Vec<u8>, bits: i16, channels: i16, little_endian: bool) -> 
     let freqs_trans: Vec<Vec<f64>> = (0..samples)
         .map(|i| freqs_flat[i * channels..(i + 1) * channels].to_vec())
         .collect();
-    let freqs: Vec<Vec<f64>> = (0..channels)
+
+    let freqs: Vec<Vec<f64>> = (0..freqs_trans[0].len())
         .map(|i| freqs_trans.iter().map(|inner| inner[i]).collect())
         .collect();
 
-    let pcm: Vec<Vec<f64>> = freqs.iter().map(|x| idct(x.clone())).collect();
+    let pcm_trans: Vec<Vec<f64>> = freqs.iter().map(|x| idct(x.clone())).collect();
 
+    let pcm: Vec<Vec<f64>> = (0..pcm_trans[0].len())
+        .map(|i| pcm_trans.iter().map(|inner| inner[i]).collect())
+        .collect();
     return pcm;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 mod fourier;
 mod tools;
 
+// use libsoxr::Soxr;
+
 fn main() {
     let pcm = vec![
-        vec![0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5],
-        vec![0.0, -0.5, -1.0, -0.5, 0.0, 0.5, 1.0, 0.5]
+        vec![0.0, 0.0],
+        vec![-1.0, 1.0],
+        vec![0.0, 0.0],
+        vec![1.0, -1.0],
+        vec![0.0, 0.0],
+        vec![-1.0, 1.0],
+        vec![0.0, 0.0],
+        vec![1.0, -1.0],
     ];
+    let channels = pcm[0].len() as i16;
     let bits = 32;
-    let channels = pcm.len() as i16;
     let little_endian = false;
     let (frad, bits) = fourier::analogue(pcm, bits, little_endian);
-    println!("{:?}", frad);
 
     let stream = tools::ecc::encode_rs(frad, 96, 24);
 
+    println!("{:?}", stream);
     // Transmission as bytestream
 
     let frad = tools::ecc::decode_rs(stream, 96, 24);
-    let restored = fourier::digital(frad, bits, channels, little_endian);
-    println!("{:?}", restored);
+    let pcm = fourier::digital(frad, bits, channels, little_endian);
+    println!("{:?}", pcm);
+
+    // let srate = 44100.0;
+    // let new_srate = 48000.0;
+    // let soxr = Soxr::create(srate, new_srate, channels as u32, None, None, None).unwrap();
+    // let mut target = vec![vec![0.0; channels as usize]; (pcm.len() as f64 * srate / new_srate) as usize];
+
+    // let _ = soxr.process(Some(&pcm), &mut target);
+    // soxr.process::<f64, _>(None, &mut target[0..]).unwrap();
+
+    // println!("{:?}", target);
 }
