f22b5361c73e76ad2d5d87414702c439c5f496d1
h4n-ul <jun061119@gmail.com>
2024-09-11 17:33:00 +0900

    UPDATE :: Using functional expression for readability and performance

--- src/backend/bitcvt.rs
+++ src/backend/bitcvt.rs
@@ -10,12 +10,9 @@
  * Returns: Bitstream
  */
 pub fn to_bits(bytes: Vec<u8>) -> Vec<bool> {
-    let mut bitstream: Vec<bool> = Vec::new();
-
-    for byte in bytes {
-        for i in 0..8 { bitstream.push((byte >> (7 - i)) & 1 == 1); }
-    }
-    return bitstream;
+    return bytes.into_iter().flat_map(|byte| {
+        (0..8).rev().map(move |i| (byte & (1 << i)) != 0)
+    }).collect();
 }
 
 /** to_bytes
@@ -24,13 +21,9 @@ pub fn to_bits(bytes: Vec<u8>) -> Vec<bool> {
  * Returns: Byte array
  */
 pub fn to_bytes(bitstream: Vec<bool>) -> Vec<u8> {
-    let mut bytes: Vec<u8> = Vec::new();
-    let mut byte: u8 = 0u8;
-
-    for (i, bit) in bitstream.iter().enumerate() {
-        if *bit { byte |= 1 << (7 - (i % 8)); }
-        if (i + 1) % 8 == 0 { bytes.push(byte); byte = 0; }
-    }
-    if bitstream.len() % 8 != 0 { bytes.push(byte); } // flushing last bits with zero padding
-    return bytes;
+    return bitstream.chunks(8).map(|byte| {
+        byte.iter().enumerate().fold(0u8, |acc, (i, &bit)| {
+            acc | ((bit as u8) << (7 - i))
+        })
+    }).collect();
 }
