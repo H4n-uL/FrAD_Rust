b1da6ad6108121748e65a354aa60783903b5274d
h4n-ul <jun061119@gmail.com>
2024-09-23 18:44:22 +0900

    MINOR :: else match -> else if let on repairer

--- src/repairer.rs
+++ src/repairer.rs
@@ -28,18 +28,16 @@ pub fn repair(rfile: String, params: CliParams) {
 
     let mut wpipe = false;
     if PIPEOUT.contains(&wfile.as_str()) { wpipe = true; }
-    else {
-        match is_same_file(&rfile, &wfile) {
-            Ok(true) => { eprintln!("Input and output files cannot be the same"); exit(1); }
-            _ => {}
-        }
-        if wfile.is_empty() {
-            let wfrf = Path::new(&rfile).file_name().unwrap().to_string_lossy().split(".").map(|s| s.to_string()).collect::<Vec<String>>();
-            wfile = [wfrf[..wfrf.len() - 1].join("."), "recov".to_string(), wfrf[wfrf.len() - 1].clone()].join(".");
-        }
-
-        check_overwrite(&wfile, params.overwrite);
+    else if let Ok(true) = is_same_file(&rfile, &wfile) {
+        eprintln!("Input and output files cannot be the same"); exit(1);
     }
+
+    if wfile.is_empty() {
+        let wfrf = Path::new(&rfile).file_name().unwrap().to_string_lossy().split(".").map(|s| s.to_string()).collect::<Vec<String>>();
+        wfile = [wfrf[..wfrf.len() - 1].join("."), "recov".to_string(), wfrf[wfrf.len() - 1].clone()].join(".");
+    }
+
+    check_overwrite(&wfile, params.overwrite);
 
     let mut readfile: Box<dyn Read> = if !rpipe { Box::new(File::open(rfile).unwrap()) } else { Box::new(std::io::stdin()) };
     let mut writefile: Box<dyn Write> = if !wpipe { Box::new(File::create(wfile).unwrap()) } else { Box::new(std::io::stdout()) };
