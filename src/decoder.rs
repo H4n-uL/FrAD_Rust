a4f10b691ee50f84bed2c393f78e101771546a7b
h4n-ul <jun061119@gmail.com>
2024-09-25 13:39:59 +0900

    UPDATE :: Updated decode file overwrite check logic

--- src/decoder.rs
+++ src/decoder.rs
@@ -41,27 +41,28 @@ fn write(isplay: bool, file: &mut Box<dyn Write>, sink: &mut Sink, pcm: Vec<Vec<
  * Returns: Decoded PCM on File or stdout
  */
 pub fn decode(rfile: String, mut params: CliParams, play: bool) {
-    let mut wfile = params.output;
+    let mut wfile_prim = params.output;
     if rfile.is_empty() { eprintln!("Input file must be given"); exit(1); }
 
     let (mut rpipe, mut wpipe) = (false, false);
     if PIPEIN.contains(&rfile.as_str()) { rpipe = true; }
     else if !Path::new(&rfile).exists() { eprintln!("Input file does not exist"); exit(1); }
-    if PIPEOUT.contains(&wfile.as_str()) { wpipe = true; }
-    else if let Ok(true) = is_same_file(&rfile, &wfile) {
+    if PIPEOUT.contains(&wfile_prim.as_str()) || play { wpipe = true; }
+    else if let Ok(true) = is_same_file(&rfile, &wfile_prim) {
         eprintln!("Input and output files cannot be the same"); exit(1);
     }
 
-    if wfile.is_empty() {
+    if wfile_prim.is_empty() {
         let wfrf = Path::new(&rfile).file_name().unwrap().to_str().unwrap().to_string();
-        wfile = wfrf.split(".").collect::<Vec<&str>>()[..wfrf.split(".").count() - 1].join(".");
+        wfile_prim = wfrf.split(".").collect::<Vec<&str>>()[..wfrf.split(".").count() - 1].join(".");
     }
-    else if wfile.ends_with(".pcm") { wfile = wfile[..wfile.len() - 4].to_string(); }
+    else if wfile_prim.ends_with(".pcm") { wfile_prim = wfile_prim[..wfile_prim.len() - 4].to_string(); }
 
-    check_overwrite(&wfile, params.overwrite);
+    let mut wfile = format!("{}.pcm", wfile_prim);
+    if wpipe { check_overwrite(&wfile, params.overwrite); }
 
     let mut readfile: Box<dyn Read> = if !rpipe { Box::new(File::open(rfile).unwrap()) } else { Box::new(std::io::stdin()) };
-    let mut writefile: Box<dyn Write> = if !wpipe && !play { Box::new(File::create(format!("{}.pcm", wfile)).unwrap()) } else { Box::new(std::io::stdout()) };
+    let mut writefile: Box<dyn Write> = if wpipe { Box::new(File::create(wfile).unwrap()) } else { Box::new(std::io::stdout()) };
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let mut sink = Sink::try_new(&stream_handle).unwrap();
     sink.set_speed(params.speed as f32);
@@ -81,8 +82,10 @@ pub fn decode(rfile: String, mut params: CliParams, play: bool) {
         write(play, &mut writefile, &mut sink, pcm, &pcm_fmt, &srate);
         logging(params.loglevel, &decoder.streaminfo, false);
 
-        if critical_info_modified && !(wpipe || play) {
-            no += 1; writefile = Box::new(File::create(format!("{}.{}.pcm", wfile, no)).unwrap());
+        if critical_info_modified && wpipe {
+            no += 1; wfile = format!("{}.{}.pcm", wfile_prim, no);
+            check_overwrite(&wfile, params.overwrite);
+            writefile = Box::new(File::create(wfile).unwrap());
         }
     }
     let (pcm, srate, _) = decoder.flush();
