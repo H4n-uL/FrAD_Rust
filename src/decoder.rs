6eabd5903beccaf3e21f7e39df5e81ed7af0bf82
h4n-ul <jun061119@gmail.com>
2024-10-07 12:33:43 +0900

    UPDATE :: Disabling sink when not playing

--- src/decoder.rs
+++ src/decoder.rs
@@ -16,22 +16,21 @@ use same_file::is_same_file;
 
 /** write
  * Writes PCM data to file or sink
- * Parameters: Play flag, Output file/sink, PCM data, PCM format, Sample rate
+ * Parameters: Output file/sink, PCM data, PCM format, Sample rate
  * Parameters: Output file, PCM data
  * Returns: None
  */
-fn write(isplay: bool, file: &mut Box<dyn Write>, sink: &mut Sink, pcm: Vec<Vec<f64>>, fmt: &PCMFormat, srate: u32) {
+fn write(file: &mut Box<dyn Write>, sink: Option<&mut Sink>, pcm: Vec<Vec<f64>>, fmt: &PCMFormat, srate: u32) {
     if pcm.is_empty() { return; }
-    if isplay {
-        sink.append(SamplesBuffer::new(
-            pcm[0].len() as u16,
-            srate,
-            pcm.into_iter().flatten().map(|x| x as f32).collect::<Vec<f32>>()
-        ));
-    }
-    else {
-        let pcm_bytes: Vec<u8> = pcm.into_iter().flatten().flat_map(|x| f64_to_any(x, fmt)).collect();
-        write_safe(file, &pcm_bytes);
+    match sink {
+        Some(s) => s.append(SamplesBuffer::new(
+                pcm[0].len() as u16, srate,
+                pcm.into_iter().flatten().map(|x| x as f32).collect::<Vec<f32>>()
+            )),
+        None => {
+            let pcm_bytes: Vec<u8> = pcm.into_iter().flatten().flat_map(|x| f64_to_any(x, fmt)).collect();
+            write_safe(file, &pcm_bytes);
+        }
     }
 }
 
@@ -89,11 +88,15 @@ pub fn decode(rfile: String, mut params: CliParams, play: bool) {
 
     let mut readfile: Box<dyn Read> = if !rpipe { Box::new(File::open(rfile).unwrap()) } else { Box::new(std::io::stdin()) };
     let mut writefile: Box<dyn Write> = if !wpipe { Box::new(File::create(wfile).unwrap()) } else { Box::new(std::io::stdout()) };
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let mut sink = Sink::try_new(&stream_handle).unwrap();
-    sink.set_speed(params.speed as f32);
 
-    if play { params.loglevel = 0; }
+    let (_stream, _stream_handle, mut sink) = if play {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        (Some(_stream), Some(stream_handle), Some(sink))
+    } else { (None, None, None) };
+
+    sink.as_mut().map(|s| { s.set_speed(params.speed as f32); params.loglevel = 0; });
+
     let mut decoder = Decoder::new(params.enable_ecc);
     let pcm_fmt = params.pcm;
 
@@ -102,10 +105,10 @@ pub fn decode(rfile: String, mut params: CliParams, play: bool) {
         let mut buf = vec![0u8; 32768];
         let readlen = read_exact(&mut readfile, &mut buf);
 
-        if readlen == 0 && decoder.is_empty() && (!play || sink.empty()) { break; }
+        if readlen == 0 && decoder.is_empty() && sink.as_ref().map_or(true, |s| s.empty()) { break; }
 
         let decoded = decoder.process(buf[..readlen].to_vec());
-        write(play, &mut writefile, &mut sink, decoded.pcm, &pcm_fmt, decoded.srate);
+        write(&mut writefile, sink.as_mut(), decoded.pcm, &pcm_fmt, decoded.srate);
         logging_decode(params.loglevel, &decoder.procinfo, false, decoder.get_asfh());
 
         if decoded.crit && !wpipe {
@@ -117,7 +120,7 @@ pub fn decode(rfile: String, mut params: CliParams, play: bool) {
         }
     }
     let decoded = decoder.flush();
-    write(play, &mut writefile, &mut sink, decoded.pcm, &pcm_fmt, decoded.srate);
+    write(&mut writefile, sink.as_mut(), decoded.pcm, &pcm_fmt, decoded.srate);
     logging_decode(params.loglevel, &decoder.procinfo, true, decoder.get_asfh());
-    if play { sink.sleep_until_end(); }
+    sink.map(|s| s.sleep_until_end());
 }
