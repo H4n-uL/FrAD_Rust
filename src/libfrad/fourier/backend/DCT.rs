5a60a139b6e2724dba66abf6489af20fa672ce8d
h4n-ul <jun061119@gmail.com>
2024-10-15 19:17:53 +0900

    UPDATE :: Updated shorter and more readable DCT-II Forward

--- src/libfrad/fourier/backend/DCT.rs
+++ src/libfrad/fourier/backend/DCT.rs
@@ -7,31 +7,19 @@
 use std::f64::consts::PI;
 
 pub fn dct(x: Vec<f64>) -> Vec<f64> {
-    let n = x.len();
-    let mut y = vec![0.0; n];
-
-    for i in 0..n {
-        let mut sum = 0.0;
-        for j in 0..n {
-            let angle = (PI / n as f64) * (j as f64 + 0.5) * i as f64;
-            sum += x[j] * angle.cos();
-        }
-        y[i] = sum / n as f64;
-    }
-    return y;
+    return (0..x.len()).map(|i| {
+        let sum= (0..x.len()).map(|j| {
+            x[j] * ((PI / x.len() as f64) * (j as f64 + 0.5) * i as f64).cos()
+        }).sum::<f64>();
+        sum / x.len() as f64
+    }).collect();
 }
 
 pub fn idct(y: Vec<f64>) -> Vec<f64> {
-    let n = y.len();
-    let mut x = vec![0.0; n];
-
-    for i in 0..n {
-        let mut sum = y[0] / 2.0;
-        for j in 1..n {
-            let angle = (PI / n as f64) * (i as f64 + 0.5) * j as f64;
-            sum += y[j] * angle.cos();
-        }
-        x[i] = sum * 2.0;
-    }
-    return x;
+    return (0..y.len()).map(|i| {
+        let sum = y[0] + (1..y.len()).map(|j| {
+            y[j] * ((PI / y.len() as f64) * (i as f64 + 0.5) * j as f64).cos() * 2.0
+        }).sum::<f64>();
+        sum
+    }).collect();
 }
