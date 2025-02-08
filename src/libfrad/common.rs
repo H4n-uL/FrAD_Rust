fbadb5a9fa0a726635050b80b6d8a6cee8674d51
H4n-uL <jun061119@gmail.com>
2025-02-08 19:07:15 +0900

    UPDATE :: Updated faster CRC algorithm

--- src/libfrad/common.rs
+++ src/libfrad/common.rs
@@ -8,20 +8,24 @@
 pub const SIGNATURE: [u8; 4] = [0x66, 0x52, 0x61, 0x64];
 pub const FRM_SIGN: [u8; 4] = [0xff, 0xd0, 0xd2, 0x97];
 
+// CRC Table sizes
+const TABLE_SIZE_CRC32: usize = 16;
+const TABLE_SIZE_CRC16_ANSI: usize = 16;
+
 // CRC-32 Table generator
-const fn gcrc32t() -> [[u32; 256]; 4] {
-    let mut tables = [[0u32; 256]; 4];
+const fn gcrc32t() -> [[u32; 256]; TABLE_SIZE_CRC32] {
+    let mut tables = [[0u32; 256]; TABLE_SIZE_CRC32];
 
     let mut i = 0; while i < 256 {
         let (mut crc, mut j) = (i as u32, 0);
         while j < 8 {
-            if crc & 1 == 1 { crc = (crc >> 1) ^ 0xedb88320; } else { crc >>= 1; }
+            crc = if crc & 1 == 1 { (crc >> 1) ^ 0xedb88320 } else { crc >> 1 };
         j += 1; }
         tables[0][i] = crc;
     i += 1; }
 
     let mut i = 0; while i < 256 {
-        let mut j = 1; while j < 4 { // table count
+        let mut j = 1; while j < TABLE_SIZE_CRC32 {
             tables[j][i] = (tables[j-1][i] >> 8) ^ tables[0][tables[j-1][i] as u8 as usize];
         j += 1; }
     i += 1; }
@@ -29,72 +33,91 @@ const fn gcrc32t() -> [[u32; 256]; 4] {
     return tables;
 }
 
-// CRC-32 Table
-const CRC32T: [[u32; 256]; 4] = gcrc32t();
-
-/** crc32
- * Calculates CRC-32 checksum of a byte array
- * Parameters: Byte array
- * Returns: CRC-32 checksum in byte array
- */
-pub fn crc32(data: &[u8]) -> Vec<u8> {
-    let mut crc = u32::MAX;
-    let chunks = data.chunks_exact(4);
-    let rem = chunks.remainder();
-
-    chunks.for_each(|chunk| {
-        crc ^= u32::from_le_bytes(chunk.try_into().unwrap());
-        crc = CRC32T[3][( crc        & 0xff) as usize] ^
-              CRC32T[2][((crc >>  8) & 0xff) as usize] ^
-              CRC32T[1][((crc >> 16) & 0xff) as usize] ^
-              CRC32T[0][((crc >> 24) & 0xff) as usize];
-    });
-
-    rem.iter().for_each(|&byte| { crc = (crc >> 8) ^ CRC32T[0][(crc ^ byte as u32) as usize]; });
-
-    return (!crc).to_be_bytes().to_vec();
-}
-
 // CRC-16 ANSI Table generator
-const fn gcrc16t_ansi() -> [[u16; 256]; 2] {
-    let mut tables = [[0u16; 256]; 2];
-    
-    // 기본 테이블 생성
+const fn gcrc16t_ansi() -> [[u16; 256]; TABLE_SIZE_CRC16_ANSI] {
+    let mut tables = [[0u16; 256]; TABLE_SIZE_CRC16_ANSI];
+
     let mut i = 0; while i < 256 {
-        let mut crc = i as u16;
-        let mut j = 0; while j < 8 {
+        let (mut crc, mut j) = (i as u16, 0);
+        while j < 8 {
             crc = if crc & 1 == 1 { (crc >> 1) ^ 0xA001 } else { crc >> 1 };
         j += 1; }
         tables[0][i] = crc;
     i += 1; }
-    
-    // 두 번째 테이블 생성
+
     let mut i = 0; while i < 256 {
-        tables[1][i] = (tables[0][i] >> 8) ^ tables[0][tables[0][i] as u8 as usize];
+        let mut j = 1; while j < TABLE_SIZE_CRC16_ANSI {
+            tables[j][i] = (tables[j-1][i] >> 8) ^ tables[0][tables[j-1][i] as u8 as usize];
+        j += 1; }
     i += 1; }
-    
+
     return tables;
 }
 
-// CRC-16 ANSI Table
-const CRC16T_ANSI: [[u16; 256]; 2] = gcrc16t_ansi();
+// CRC Tables
+const CRC32_TABLE: [[u32; 256]; TABLE_SIZE_CRC32] = gcrc32t();
+const CRC16T_ANSI: [[u16; 256]; TABLE_SIZE_CRC16_ANSI] = gcrc16t_ansi();
 
-/** crc16_ansi
+/** crc32_slow
+ * Calculates CRC-32 checksum of a byte array
+ * Parameters: Byte array
+ * Returns: CRC-32 checksum
+ */
+fn crc32_slow(mut crc: u32, buf: &[u8]) -> u32 {
+    crc = !crc;
+    buf.iter().for_each(|&byte| { crc = (crc >> 8) ^ CRC32_TABLE[0][((crc as u8) ^ byte) as usize]; });
+    return !crc;
+}
+
+/** crc16_ansi_slow
  * Calculates CRC-16 ANSI checksum of a byte array
  * Parameters: Byte array
- * Returns: CRC-16 ANSI checksum in byte array
+ * Returns: CRC-16 ANSI checksum
  */
-pub fn crc16_ansi(data: &[u8]) -> Vec<u8> {
-    let mut crc = 0u16;
-    let chunks = data.chunks_exact(2);
-    let rem = chunks.remainder();
+fn crc16_ansi_slow(mut crc: u16, buf: &[u8]) -> u16 {
+    buf.iter().for_each(|&byte| { crc = (crc >> 8) ^ CRC16T_ANSI[0][((crc as u8) ^ byte) as usize]; });
+    return crc;
+}
 
-    chunks.for_each(|chunk| {
-        crc ^= u16::from_le_bytes(chunk.try_into().unwrap());
-        crc = CRC16T_ANSI[1][(crc & 0xff) as usize] ^
-              CRC16T_ANSI[0][((crc >> 8) & 0xff) as usize];
+/** crc32
+ * Accelerated CRC-32 checksum calculation
+ * Parameters: Byte array
+ * Returns: CRC-32 checksum
+ */
+pub fn crc32(mut crc: u32, buf: &[u8]) -> u32 {
+    if TABLE_SIZE_CRC32 < 4 { return crc32_slow(crc, buf); }
+    crc = !crc;
+
+    buf.chunks(TABLE_SIZE_CRC32).for_each(|chunk| {
+        if chunk.len() < TABLE_SIZE_CRC32 { crc = !crc32_slow(!crc, chunk); return; }
+        let mut crc_temp = 0u32;
+        for i in (0..TABLE_SIZE_CRC32).rev() {
+            if i < 4 { crc_temp ^= CRC32_TABLE[TABLE_SIZE_CRC32 - i - 1][chunk[i] as usize ^ ((crc >> (i * 8)) & 0xFF) as usize]; }
+            else { crc_temp ^= CRC32_TABLE[TABLE_SIZE_CRC32 - i - 1][chunk[i] as usize]; }
+        }
+        crc = crc_temp;
     });
 
-    rem.iter().for_each(|&byte| { crc = (crc >> 8) ^ CRC16T_ANSI[0][((crc ^ byte as u16) & 0xff) as usize]; });
-    return crc.to_be_bytes().to_vec();
+    return !crc;
+}
+
+/** crc16_ansi
+ * Accelerated CRC-16 ANSI checksum calculation
+ * Parameters: Byte array
+ * Returns: CRC-16 ANSI checksum
+ */
+pub fn crc16_ansi(mut crc: u16, buf: &[u8]) -> u16 {
+    if TABLE_SIZE_CRC16_ANSI < 2 { return crc16_ansi_slow(crc, buf); }
+
+    buf.chunks(TABLE_SIZE_CRC16_ANSI).for_each(|chunk| {
+        if chunk.len() < TABLE_SIZE_CRC16_ANSI { crc = crc16_ansi_slow(crc, chunk); return; }
+        let mut crc_temp = 0u16;
+        for i in (0..TABLE_SIZE_CRC16_ANSI).rev() {
+            if i < 2 { crc_temp ^= CRC16T_ANSI[TABLE_SIZE_CRC16_ANSI - i - 1][chunk[i] as usize ^ ((crc >> (i * 8)) as u8 as usize)]; }
+            else { crc_temp ^= CRC16T_ANSI[TABLE_SIZE_CRC16_ANSI - i - 1][chunk[i] as usize]; }
+        }
+        crc = crc_temp;
+    });
+
+    return crc;
 }
