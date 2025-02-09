d561758bb832098fab2542c5a0970f9017fad89c
H4n-uL <jun061119@gmail.com>
2025-02-09 15:49:24 +0900

    DOC :: I{bits}{endian} to S{bits}{endian}

--- src/libfrad/backend/pcmformat.rs
+++ src/libfrad/backend/pcmformat.rs
@@ -7,18 +7,18 @@
 #[derive(Clone, Copy)]
 pub enum PCMFormat {
     F16BE, F16LE, F32BE, F32LE, F64BE, F64LE,
-    I8, I16BE, I16LE, I24BE, I24LE, I32BE, I32LE, I64BE, I64LE,
+    S8, S16BE, S16LE, S24BE, S24LE, S32BE, S32LE, S64BE, S64LE,
     U8, U16BE, U16LE, U24BE, U24LE, U32BE, U32LE, U64BE, U64LE
 }
 
 impl PCMFormat {
     pub fn bit_depth(&self) -> usize {
         match self {
-            Self::I8 | Self::U8 => 8,
-            Self::F16BE | Self::F16LE | Self::I16BE | Self::I16LE | Self::U16BE | Self::U16LE => 16,
-                                        Self::I24BE | Self::I24LE | Self::U24BE | Self::U24LE => 24,
-            Self::F32BE | Self::F32LE | Self::I32BE | Self::I32LE | Self::U32BE | Self::U32LE => 32,
-            Self::F64BE | Self::F64LE | Self::I64BE | Self::I64LE | Self::U64BE | Self::U64LE => 64
+            Self::S8 | Self::U8 => 8,
+            Self::F16BE | Self::F16LE | Self::S16BE | Self::S16LE | Self::U16BE | Self::U16LE => 16,
+                                        Self::S24BE | Self::S24LE | Self::U24BE | Self::U24LE => 24,
+            Self::F32BE | Self::F32LE | Self::S32BE | Self::S32LE | Self::U32BE | Self::U32LE => 32,
+            Self::F64BE | Self::F64LE | Self::S64BE | Self::S64LE | Self::U64BE | Self::U64LE => 64
         }
     }
     pub fn float(&self) -> bool {
@@ -29,11 +29,11 @@ impl PCMFormat {
     }
     pub fn scale(&self) -> f64 {
         match self {
-            Self::I8 | Self::U8 => 128.0,
-            Self::I16BE | Self::I16LE | Self::U16BE | Self::U16LE => 32768.0,
-            Self::I24BE | Self::I24LE | Self::U24BE | Self::U24LE => 8388608.0,
-            Self::I32BE | Self::I32LE | Self::U32BE | Self::U32LE => 2147483648.0,
-            Self::I64BE | Self::I64LE | Self::U64BE | Self::U64LE => 9223372036854775808.0,
+            Self::S8 | Self::U8 => 128.0,
+            Self::S16BE | Self::S16LE | Self::U16BE | Self::U16LE => 32768.0,
+            Self::S24BE | Self::S24LE | Self::U24BE | Self::U24LE => 8388608.0,
+            Self::S32BE | Self::S32LE | Self::U32BE | Self::U32LE => 2147483648.0,
+            Self::S64BE | Self::S64LE | Self::U64BE | Self::U64LE => 9223372036854775808.0,
             _ => 1.0
         }
     }
