010c23c0b17e16f2660f60cd83a66eb591f0720d
H4n-uL <jun061119@gmail.com>
2025-02-12 12:12:13 +0900

    UPDATE :: Updated DCT as geometric operations

--- src/libfrad/fourier/backend/core.rs
+++ src/libfrad/fourier/backend/core.rs
@@ -1,9 +1,8 @@
-/**                    Fast Discrete Cosine Transform - II                    */
-/**
- * Copyright 2024 HaמuL
- * Description: FCT-II, Forward normalised
- * Dependencies: palmfft
- */
+///                    Fast Discrete Cosine Transform - II                   ///
+///
+/// Copyright 2024 HaמuL
+/// Description: FCT-II, Forward normalised
+/// Dependencies: palmfft
 
 use core::f64::consts::PI;
 use palmfft::{CfftPlan, Complex};
@@ -11,24 +10,17 @@ use palmfft::{CfftPlan, Complex};
 pub fn dct(x: Vec<f64>) -> Vec<f64> {
     let n = x.len();
 
-    let alpha = (0..n).map(|i| Complex::new(x[i] / (2.0 * n as f64), 0.0));
+    let alpha = (0..n).map(|i| Complex::new(x[i], 0.0));
     let mut beta: Vec<Complex> = alpha.clone().chain(alpha.rev()).collect();
-    CfftPlan::new(2 * n).forward(&mut beta, 1.0);
+    CfftPlan::new(2 * n).forward(&mut beta, 1.0 / (2.0 * n as f64));
 
-    return (0..n).map(|k| {
-        let angle = -PI * k as f64 / (2.0 * n as f64);
-        beta[k].r * angle.cos() - beta[k].i * angle.sin()
-    }).collect();
+    return (0..n).map(|k| beta[k].conj().dot(Complex::from_polar(1.0, -PI * k as f64 / (2.0 * n as f64)))).collect();
 }
 
 pub fn idct(y: Vec<f64>) -> Vec<f64> {
     let n = y.len();
 
-    let alpha: Vec<Complex> = (0..n).map(|k| {
-        let angle = -PI * k as f64 / (2.0 * n as f64);
-        Complex::new(y[k] * angle.cos(), y[k] * angle.sin())
-    }).collect();
-
+    let alpha: Vec<Complex> = (0..n).map(|k| Complex::from_polar(y[k], -PI * k as f64 / (2.0 * n as f64))).collect();
     let mut beta: Vec<Complex> = alpha.iter().map(|&z| z.conj())
     .chain([Complex::new(0.0, 0.0)]).chain(alpha[1..].iter().rev().cloned()).collect();
 
