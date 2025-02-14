78b20c0529743a57b6934ed0aeb9bcf4b6bbe98b
H4n-uL <jun061119@gmail.com>
2025-02-14 19:49:51 +0900

    DOC :: More concise DCT

--- src/libfrad/fourier/backend/DCT.rs
+++ src/libfrad/fourier/backend/DCT.rs
@@ -5,20 +5,14 @@
 
 use core::f64::consts::PI;
 
-pub fn dct(x: Vec<f64>) -> Vec<f64> {
+pub fn dct(x: &[f64]) -> Vec<f64> {
     return (0..x.len()).map(|i| {
-        let sum = (0..x.len()).map(|j| {
-            x[j] * ((PI / x.len() as f64) * (j as f64 + 0.5) * i as f64).cos()
-        }).sum::<f64>();
-        sum / x.len() as f64
+        (0..x.len()).map(|j| x[j] * ((PI / x.len() as f64) * (j as f64 + 0.5) * i as f64).cos()).sum::<f64>() / x.len() as f64
     }).collect();
 }
 
-pub fn idct(y: Vec<f64>) -> Vec<f64> {
+pub fn idct(y: &[f64]) -> Vec<f64> {
     return (0..y.len()).map(|i| {
-        let sum = y[0] + (1..y.len()).map(|j| {
-            y[j] * ((PI / y.len() as f64) * (i as f64 + 0.5) * j as f64).cos() * 2.0
-        }).sum::<f64>();
-        sum
+        y[0] + (1..y.len()).map(|j| y[j] * ((PI / y.len() as f64) * (i as f64 + 0.5) * j as f64).cos() * 2.0).sum::<f64>() 
     }).collect();
 }
